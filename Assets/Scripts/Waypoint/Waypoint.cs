5a6b7f392eba6aa9bf3f2db9399d9b1be6d2b64e
Mark McArthey <mcarthey@gmail.com>
2022-01-02 09:48:24 -0600

    Project setup; Includes beginning of Waypoint class and WaypointEditor

---
+++ Assets/Scripts/Waypoint/Waypoint.cs
@@ -0,0 +1,44 @@
+using UnityEngine;
+
+public class Waypoint : MonoBehaviour
+{
+    private Vector3 _currentPosition;
+    private bool _gameStarted;
+    [SerializeField] private Vector3[] points;
+
+    public Vector3[] Points => points;
+
+    public Vector3 CurrentPosition => _currentPosition;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        _gameStarted = true;
+        _currentPosition = transform.position;
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+    }
+
+    private void OnDrawGizmos()
+    {
+        if (!_gameStarted && transform.hasChanged)
+        {
+            _currentPosition = transform.position;
+        }
+
+        for (var i = 0; i < points.Length; i++)
+        {
+            Gizmos.color = Color.green;
+            Gizmos.DrawWireSphere(points[i] + _currentPosition, 0.5f);
+
+            if (i < points.Length - 1)
+            {
+                Gizmos.color = Color.gray;
+                Gizmos.DrawLine(points[i] + _currentPosition, points[i + 1] + _currentPosition);
+            }
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+# Unity-Tower-Defense
